a0c75ea95656a3b1b125f20e988f61bdc6537241
RohitBeniwal <beniwalrb177@gmail.com>
2023-08-25 16:24:19 +0530

    new update

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {Box,Button,Stack,TextField,Typography} from '@mui/material'
 
 const SearchExercises = () => {
   return (
-    <div>
-      
-    </div>
+    <Stack alignItems="center" mt='37px' justifyContent='center' p='20px'>
+      <Typography fontWeight={700}>
+        Awesome Exercises You <br/> Should Know
+      </Typography>
+    </Stack>
   )
 }
 
