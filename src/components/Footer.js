1de48efb8dc21907e2c5e94a00e915c07f2a20b5
RohitBeniwal <beniwalrb177@gmail.com>
2023-09-05 07:42:47 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <Box mt='80px' bgcolor='#fff3f4'>
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
-        <img src={Logo} alt="Logo" width='200px' height='40px'/>
+        <img src={Logo} alt="Logo" width='200px' height='150px'/>
         <Typography variant='h5' pb='40px' mt='40px'>
           Made with 💖 by Rohit Beniwal
         </Typography>
